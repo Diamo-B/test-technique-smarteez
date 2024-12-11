a6842be70fb58619ddac4a156b79e0fa7f3ccc1d
bachar <diamobachar06@gmail.com>
2024-12-11 21:23:05 +0100

    enhance(UI): add responsive support

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
   }
 
   return (
-    <div className="h-full w-full flex justify-center items-center">
-      <div className='w-1/3 flex flex-col items-center gap-10 py-10'>
+    <div className="h-full w-full flex flex-col justify-center md:items-center">
+      <div className='flex flex-col items-center gap-10 md:w-[35%]'>
         <h1 className={`text-8xl font-medium ${count > 0 ? "text-blue-600" : count < 0 ? "text-red-600" : "text-black"}`} data-testid="count">{count}</h1>
         <div className="flex w-full justify-evenly">
           <Button
